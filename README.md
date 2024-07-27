568fe210233add721362caf71984f6957a3c25f6
Russel rex <russelrex_23@yahoo.com>
2024-07-28 00:25:26 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Getting Started
+
+Open terminal and clone the repository
+
+```bash
+git clone <WEB URL to the repository>
+```
+
+CD to the folder and run
+
+```bash
+npm install
+```
+
+Then run
+
+```bash
+node server.js
+```
+
+API is running on port 5001
---
+++ server.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const cors = require('cors');
+
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+app.use(cors());
+const port = 5001;
+
+const API_KEY = process.env.API_KEY;
+const WHOIS_API_URL = 'https://www.whoisxmlapi.com/whoisserver/WhoisService';
+
+app.get('/api/whois', async (req, res) => {
+    const { domainName } = req.query;
+
+    if (!domainName) {
+        return res.status(400).json({ error: 'Domain is required' });
+    }
+
+    try {
+        const response = await axios.get(WHOIS_API_URL, {
+            params: {
+                apiKey: API_KEY,
+                domainName,
+                outputFormat: "JSON"
+            }
+        });
+        
+        res.status(200).json(response.data);
+    } catch (error) {
+        console.error('Error fetching WHOIS data:', error);
+        res.status(500).json({ error: 'Failed to fetch WHOIS data' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
